78b253113065d22aca5ebc489dd6b3bc2272c3a1
qixin <379281867@qq.com>
2019-06-28 10:13:54 +0800

    first commit

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
@@ -19,8 +19,43 @@ function App() {
           Learn React
         </a>
       </header>
+      <Clock />
     </div>
   );
+}
+
+class Clock extends Component{
+  constructor(props) {
+    super(props);
+    this.state = {date: new Date()};
+  }
+
+  componentDidMount() {
+    this.timerID = setInterval(
+      () => this._tick(),
+      1000
+    );
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.timerID);
+  }
+
+  _tick(){
+    this.setState({
+      date: new Date()
+    });
+  }
+
+
+  render(){
+    return (
+      <div>
+        <h1>Hello, world!</h1>
+        <h2>It is {this.state.date.toLocaleTimeString()}.</h2>
+      </div>
+    );
+  }
 }
 
 export default App;
